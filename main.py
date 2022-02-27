aefcfb434b05229b6d68e026e314511406ffbb7b
XueyanQiu <879561967@qq.com>
2022-02-27 18:12:26 +0800

    task：wZRY脚本 cause：每日任务自动化 solution:截图资源文件上传；截图资源使用相对路径

--- main.py
+++ main.py
@@ -314,7 +314,7 @@ def rank_meeting_challenge_once():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    imgDocumentPath = 'D:\\pyDocuments\\img\\'
+    imgDocumentPath = './img/'
     listenThreadRunning = False
     get_emails()
     getDajiReward()
